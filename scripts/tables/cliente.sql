41948b84e2fe2b561c4e798499572e48773b1f7d
HenriqueNoHub <gheds2006@gmail.com>
2025-12-16 16:34:23 -0300

    'Cliente' table has been created

---
+++ scripts/tables/cliente.sql
@@ -0,0 +1,16 @@
+/*
+  Arquivo: scripts/tables/cliente.sql
+  Objetivo: Criar a tabela de Clientes
+*/
+
+create table if not exists cliente (
+    cpf varchar(11) not null,
+    nome varchar(64) not null,
+    dataNascimento date not null,
+    endereco varchar(128),
+    telefone varchar(20),
+    email varchar(64),
+    criadoEm datetime not null default current_timestamp,
+    atualizadoEm datetime not null default current_timestamp on update current_timestamp,
+    primary key (cpf)
+);
