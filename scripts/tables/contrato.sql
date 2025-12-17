01c93c2c473845c2bd967dbbcbcb86ba70963965
Heitor-C-S <heitorcsantana.1811@gmail.com>
2025-12-16 22:44:11 -0300

    fix/auto_increment em contrato.sql

--- scripts/tables/contrato.sql
+++ scripts/tables/contrato.sql
@@ -7,7 +7,7 @@
 
 create table if not exists contrato (
     
-    idContrato serial not null,
+    idContrato not null auto_increment,
     idCliente int not null,
     idConsorcio int not null,
     dataInicio date not null,
@@ -15,5 +15,7 @@ create table if not exists contrato (
     parcelasPagas int not null default 0,
     status varchar(20) not null,
     idVendedor char(11) not null,
+    criadoEm datetime not null default current_timestamp,
+    atualizadoEm datetime not null default current_timestamp on update current_timestamp,
     primary key(idContrato)
 );
